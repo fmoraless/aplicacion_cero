c7277cc9b339125b590bf59190821b279255fe1b
franticzero <franticzero@gmail.com>
2016-09-29 03:35:50 +0000

    Variables, condicionales y bucles

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -11,10 +11,19 @@ class PruebasController extends Controller
    
     public function indexAction(Request $request,$name,$page)
     {
-        return $this->redirect($this->generaeUrl("helloWorld"));
+        // return $this->redirect($this->generaeUrl("helloWorld"));
+        $productos = array( 
+                            array("producto"=>"Consola", "precio"=>2),
+                            array("producto"=>"Consola", "precio"=>4),
+                            array("producto"=>"Consola", "precio"=>5),
+                            array("producto"=>"Consola", "precio"=>6),
+                          );
+        $fruta = array("manzana"=>"golden","pera"=>"rica");
         // replace this example code with whatever you need
         return $this->render('AppBundle:pruebas:index.html.twig', array(
-                'texto' => $name." - ".$page
+                'texto' => $name." - ".$page,
+                'productos' => $productos,
+                'fruta' => $fruta
             ));
     }
     
