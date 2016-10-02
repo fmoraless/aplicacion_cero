d1f9d4a391c047e45d93fdee47e61bf86ac51adb
franticzero <franticzero@gmail.com>
2016-10-02 22:16:51 +0000

    cracion SQL para base Blog

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Curso;
 
 class PruebasController extends Controller
 {
@@ -27,5 +28,36 @@ class PruebasController extends Controller
             ));
     }
     
+    public function createAction() {
+        
+        $curso = new Curso();
+        $curso->setTitulo("Curso de Symfony3");
+        $curso->setDescripcion("Curso de SymfonyCurso completo de Symfony");
+        $curso->setPrecio(80);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($curso);
+        $flush=$em->flush();
+        
+        if($flush != null) {
+            echo "El curso no se ha creado bien";
+        }else{
+            echo "El curso se ha creado correctamente";
+        }
+        die();
+    }
+    
+    public function readAction(){
+        $em = $this->getDoctrine()->getManager();
+        $cursos_repo =$em->getRepository("AppBundle:Curso");
+        $cursos = $cursos_repo->findAll();
+        
+        foreach($cursos as $curso){
+            echo $curso->getTitulo()."<br/>";
+            echo $curso->getDescription()."<br/>";
+            echo $curso->getPrecio()."<br/>";
+        }
+        die();
+    }
     
 }
